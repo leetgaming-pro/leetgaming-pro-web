6203cae2ba65a4524e5fe99db6e93ca771f64cc9
Matheus Bin <matheus.voalide@hotmail.com>
2025-02-27 08:08:57 -0300

    wip

---
+++ src/components/default-layout/default-layout.tsx
@@ -0,0 +1,82 @@
+/* eslint-disable @next/next/no-head-element */
+"use client"
+import "@/styles/globals.css";
+import { Metadata, Viewport } from "next";
+import { siteConfig } from "@/config/site";
+import { fontSans, pressStart2P } from "@/config/fonts";
+import { Providers } from "./providers";
+import { Navbar } from "@/components/navbar";
+import clsx from "clsx";
+import Box from './box';
+import FooterColumns from '../footer-columns/app';
+import { useEffect, useState } from "react";
+
+export const metadata: Metadata = {
+  title: {
+    default: siteConfig.name,
+    template: `%s - ${siteConfig.name}`,
+  },
+  description: siteConfig.description,
+  icons: {
+    icon: "/favicon.ico",
+    shortcut: "/favicon-16x16.png",
+    apple: "/apple-touch-icon.png",
+  },
+};
+
+const viewport: Viewport = {
+  colorScheme: 'dark',
+  themeColor: [
+    { media: "(prefers-color-scheme: light)", color: "white" },
+    { media: "(prefers-color-scheme: dark)", color: "black" },
+  ],
+  width: 'device-width',
+  initialScale: 1,
+};
+
+// Exporting this way to avoid NextJs 14 type error
+export { viewport };
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+
+  const [domLoaded, setDomLoaded] = useState(false);
+
+  useEffect(() => {
+    setDomLoaded(true);
+  }, []);
+
+  return (
+
+    <html lang="en" suppressHydrationWarning={true}>
+
+      <head />
+      <body
+        className={clsx(
+          "min-h-screen bg-background bg-scroll blur-glow-pry-gh antialiased w-full",
+          pressStart2P.className
+        )}
+      >
+      {domLoaded && (
+          <Box>
+            <Providers themeProps={{ attribute: "class", defaultTheme: "dark"}} >
+              <div className="relative flex flex-col h-screen w-full">
+                <Navbar  />
+                {/* <main className="container mx-auto max-w-7xl pt-16 px-6 flex-grow"> */}
+                <main className="flex w-full justify-center">
+                  {children}
+                </main>
+                 <FooterColumns />
+              </div>
+            </Providers>
+          </Box>
+        )
+      }
+      </body>
+    </html>
+
+  );
+}
---
+++ src/components/navbar.tsx
@@ -0,0 +1,194 @@
+import {
+  Navbar as NextUINavbar,
+  NavbarContent,
+  NavbarMenu,
+  NavbarMenuToggle,
+  NavbarBrand,
+  NavbarItem,
+  NavbarMenuItem,
+} from "@nextui-org/navbar";
+import { Link } from "@nextui-org/link";
+
+import { link as linkStyles } from "@nextui-org/theme";
+
+import { siteConfig } from "@/config/site";
+import NextLink from "next/link";
+import clsx from "clsx";
+
+import { ThemeSwitch } from "@/components/theme-switch";
+import {
+  TwitterIcon,
+  GithubIcon,
+  DiscordIcon,
+  SearchIcon,
+  Logo,
+  SteamIcon,
+  LogOutIcon,
+  HeartFilledIcon,
+  StarredIcon,
+} from "@/components/icons";
+
+import { logo, title } from './primitives';
+import { LoginButton } from './login-button';
+import SessionButton from './session-button';
+import { useSession } from 'next-auth/react';
+import SearchInput from "./search/search-modal/search-modal";
+
+import DefaultLogo from './logo/logo-default';
+import { useTheme } from "next-themes";
+import { electrolize } from "@/config/fonts";
+import { Chip } from "@nextui-org/react";
+
+export const Navbar = () => {
+
+  let { theme, setTheme } = useTheme();
+
+  if (theme === null || theme === undefined) {
+    theme = "dark";
+  }
+
+  let SessionArea;
+
+  const { data: session } = useSession()
+
+  // console.log('##session##', JSON.stringify(session))
+  if (session) {
+    SessionArea = SessionButton;
+  } else {
+    SessionArea = LoginButton;
+  }
+
+  const searchInput = SearchInput();
+
+  return (
+    <NextUINavbar maxWidth="full" className="z-10" height={16} position="sticky" isBordered={true} isBlurred={true} style={{
+      backgroundImage: `url('/blur-glow-pry-gh.svg')`,
+      // backgroundSize: "cover",
+      // backgroundColor: "rgba(0, 0, 0, 0.5)",
+    }}>
+      <NavbarContent className="basis-1/5 sm:basis-full">
+        <NavbarBrand as="li" className=" max-w-fit">
+          <NextLink className="lg:flex flex justify-left items-left align-left" href="/">
+            {/* <Logo /> */}
+
+            <DefaultLogo  onClick={ () => window.location.href = "/" } />
+            {/* <Chip
+              variant="shadow"
+              classNames={{
+                base: "bg-gradient-to-br from-red-500 to-violet-500 border-small border-white/50 shadow-red-500/30",
+                content: "drop-shadow shadow-black text-white",
+              }}
+
+              style={{ fontSize: "0.6rem", margin: "0.0rem 0.2rem", height: "0.9rem", maxWidth: "0.2rem"}}
+            >
+             <strong>Beta</strong>
+
+            </Chip> */}
+
+          </NextLink>
+          {/* <NavBarSection /> */}
+        </NavbarBrand>
+        <ul className="hidden lg:flex justify-start ml-2 h-16 justify-center align-items align-center">
+          {siteConfig.navItems.map((item) => (
+            <NavbarItem key={item.href}
+            style={{
+              height: "100%",
+              backgroundColor: item.href === "/match-making" ? "#DCFF37" : (item.href === "/cloud" ? "#34445C" : ""),
+              minWidth: "105px",
+              display: "flex",
+              justifyContent: "center",
+              alignItems: "center",
+              fontSize: "13px",
+              color: item.href === "/match-making" ? "rgb(52, 68, 92)" : (item.href === "/cloud" ? "#F5F5F5" : ""),
+            }}
+            >
+              <NextLink
+                className={clsx(
+                  "data-[active=true]:text-primary data-[active=true]:font-medium justify-center align-items align-center w-full text-center " + electrolize.className
+                )}
+
+                href={item.href}
+
+                onClick={ () => window.location.href = item.href }
+              >
+                {item.label}
+              </NextLink>
+            </NavbarItem>
+          ))}
+        </ul>
+      </NavbarContent>
+
+      <NavbarContent
+        className="hidden sm:flex basis-1/5 sm:basis-full"
+        justify="end"
+      >
+        <NavbarItem className="hidden sm:flex gap-2">
+          {/* <Link isExternal href={siteConfig.links.twitter} aria-label="Twitter">
+            <TwitterIcon className="text-default-500" />
+          </Link> */}
+          {/* <Link isExternal href={siteConfig.links.discord} aria-label="Discord">
+            <DiscordIcon className="text-default-500" />
+          </Link> */}
+          {/* <Link isExternal href={siteConfig.links.github} aria-label="Github">
+            <GithubIcon className="text-default-500" />
+          </Link> */}
+          <ThemeSwitch />
+        </NavbarItem>
+        <NavbarItem className="hidden lg:flex">{searchInput}</NavbarItem>
+
+
+        {/* sponsor button! */}
+        {/* <NavbarItem className="hidden md:flex">
+					<Button
+            isExternal
+						as={Link}
+						className="text-sm font-normal text-default-600 bg-default-100"
+						href={siteConfig.links.sponsor}
+						startContent={<HeartFilledIcon className="text-danger" />}
+						variant="flat"
+					>
+						Sponsor
+					</Button>
+				</NavbarItem>  */}
+
+
+        <NavbarItem className="hidden lg:flex">
+          <SessionArea />
+        </NavbarItem>
+
+
+      </NavbarContent>
+
+      <NavbarContent className="sm:hidden basis-1" justify="end">
+        <Link isExternal href={siteConfig.links.github} aria-label="Github">
+          <GithubIcon className="text-default-500" />
+        </Link>
+        <ThemeSwitch />
+        <NavbarMenuToggle />
+      </NavbarContent>
+
+      <NavbarMenu>
+        {searchInput}
+        <div className="mx-4 mt-2 flex flex-col">
+          {siteConfig.navMenuItems.map((item, index) => (
+            <NavbarMenuItem key={`${item}-${index}`} className="h-4">
+              <Link
+                color={
+                  index === 2
+                    ? "primary"
+                    : index === siteConfig.navMenuItems.length - 1
+                      ? "danger"
+                      : "foreground"
+                }
+                href="#"
+                size="sm"
+              >
+                {item.label}
+              </Link>
+            </NavbarMenuItem>
+          ))}
+        </div>
+      </NavbarMenu>
+    </NextUINavbar>
+  );
+};
